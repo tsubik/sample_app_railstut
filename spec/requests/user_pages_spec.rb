3668838dc7bb931d63246ebbd98eaa0f8cba5f9f
tsubik <tsubik@gmail.com>
2013-01-14 22:46:57 +0100

    additional tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,6 +19,19 @@ describe "User pages" do
  			it "should not create a user" do
  				expect { click_button submit }.not_to change(User, :count)
  			end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector "title", text: "Sign up" }
+        it { should have_content "error" }
+        it { should have_content "* Name can't be blank" }
+        it { should have_content "* Email can't be blank" }
+        it { should have_content "* Email is invalid" }
+        it { should have_content "* Password can't be blank" }
+        it { should have_content "* Password is too short (minimum is 6 characters)" }
+        it { should have_content "* Password confirmation can't be blank" }
+      end
  		end
 
  		describe "with valid information" do
@@ -32,6 +45,14 @@ describe "User pages" do
  			it "should create a user" do
  				expect { click_button submit }.to change(User, :count).by(1)
  			end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email("tsubik@gmail22.com") }
+
+        it { should have_selector('title', text: user.name)}
+        it { should have_selector('div.alert.alert-success', text: "Welcome") }
+      end
  		end
   end
 
@@ -42,4 +63,6 @@ describe "User pages" do
 		it { should have_selector "h1", text: user.name }
 		it { should have_selector "title", text: user.name }  	
   end
+
+
 end
