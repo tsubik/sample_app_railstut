31b2a8818a6e1cac3bca5bae494915a4627902a9
tsubik <tsubik@gmail.com>
2013-01-14 20:09:36 +0100

    sign up page form

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,4 +8,30 @@ describe "User pages" do
   	it { should have_selector "h1", title: "Sign up" }
   	it { should have_selector "title", title: full_title("Sign up") }
   end
+
+  describe "signup" do
+ 		before { visit signup_path }
+
+ 		let(:submit) { "Create my account" }
+
+ 		describe "with invalid information" do
+ 			it "should not create a user" do
+ 				expect { click_button submit }.not_to change(User, :count)
+ 			end
+ 		end
+
+ 		describe "with valid information" do
+ 			before do
+ 				fill_in "Name", with: "Tomasz Subik"
+ 				fill_in "Email", with: "tsubik@gmail.com"
+ 				fill_in "Password", with: "z dupy"
+ 				fill_in "Confirmation", with: "z dupy"
+ 			end
+
+ 			it "should create a user" do
+ 				expect { click_button submit }.to change(User, :count).by(1)
+ 			end
+ 		end
+  end
+
 end
