56ffa1bd55020534921f282628a9298d390c235e
tsubik <tsubik@gmail.com>
2013-01-16 01:26:51 +0100

    finishing sign in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
   # match '/help', to: 'static_pages#help' 
   # match '/about', to: 'static_pages#about'
   # match '/contact', to: 'static_pages#contact'
   match "signup" => "users#new"
+  match "signin" => "sessions#new"
+  match "signout" => "sessions#destroy", :via => :delete
   match "help" => "static_pages#help"
   match "about" => 'static_pages#about'
   match "contact" => 'static_pages#contact'
